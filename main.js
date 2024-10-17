4af7fddf2a11879b88fe87747020cbd02ee17bbf
lynnliang34 <lynnliang34@gmail.com>
2024-10-18 00:25:37 +0800

    feat：新增 header 手機版 JS 功能
    1. 點擊放大鏡圖示，顯示搜尋框
    2. 頁面連結收合

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+// 導覽列手機版
+// 點擊放大鏡圖示，顯示搜尋框
+$(".search-icon").click(function (event) {
+  $(".search-form").toggleClass("d-none");
+});
+
+$(document).click(function (event) {
+  // 如果點擊的目標不在搜尋框或放大鏡內
+  if (!$(event.target).closest(".search-form, .search-icon").length) {
+    // 隱藏搜尋框
+    $(".search-form").addClass("d-none");
+  }
+});
+
+// 頁面連結收合
+$(".modal-link-1").click(function (event) {
+  $(this).next(".modal-link-1ist").toggleClass("d-none");
+  $(this).find(".modal-link-arrow").toggleClass("rotate");
+
+  if ($(".modal-link-1").not($(this)).next(".modal-link-1ist").not(".d-none")) {
+    $(".modal-link-1").not($(this)).next(".modal-link-1ist").addClass("d-none");
+    $(".modal-link-1")
+      .not($(this))
+      .find(".modal-link-arrow")
+      .addClass("rotate");
+  }
+});
