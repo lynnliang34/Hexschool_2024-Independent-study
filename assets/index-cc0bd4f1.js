c8c08e98a9c5e322edb3977020015a69a2371476
lynnliang34 <lynnliang34@gmail.com>
2025-03-07 14:30:43 +0800

    Updates

---
+++ assets/index-cc0bd4f1.js
@@ -0,0 +1,1 @@
+import"./main-4ff9cf6f.js";new Swiper(".swiper",{direction:"horizontal",loop:!0,speed:8e3,autoplay:{delay:0,disableOnInteraction:!1},freeMode:!0,slidesPerView:2.2,spaceBetween:8,breakpoints:{768:{slidesPerView:2.8,spaceBetween:20},992:{slidesPerView:3.5,spaceBetween:32}}});
